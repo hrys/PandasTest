cdf3277312a3baac57057ff2e592c6fb452f47a4
hrys <hrys1152@gmail.com>
2022-05-18 00:40:36 +0900

    コンバートテスト追加

---
+++ convert.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df_data01 = pd.read_excel("data01.xlsx", index_col=0)
+df_data02 = pd.read_excel("data02.xlsx", index_col=0)
+
+df_data01.to_csv("data01.csv")
+df_data02.to_csv("data02.csv")
+# df_data01.to_csv("data01.csv", index=False)
+# df_data02.to_csv("data02.csv", index=False)
+
+df_data01.to_json("data01.json")
+df_data01.to_html("data01.html")
+df_data01.to_markdown("data01.md")
+
+df_data01.to_xml("data01.xml")
