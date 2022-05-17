9f7588d1a31f32967e5efbb36dd0892a8f550b62
hrys <hrys1152@gmail.com>
2022-05-18 00:42:07 +0900

    独自チェック処理の関数化・インデックスのチェック

---
+++ validate03.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import pandera as pa
+
+df_data01 = pd.read_excel("data01.xlsx", index_col=0)
+df_data02 = pd.read_excel("data02.xlsx", index_col=0)
+
+def value04_validate(values: pd.Series, find_df:pd.DataFrame) -> (pd.Series | bool):
+    return values.apply(lambda id: not find_df.query(f'id == ["{id}"]').empty)
+
+data01_schema = pa.DataFrameSchema(
+    index = pa.Index(
+        str,
+        pa.Check(lambda x: x.str.startswith("A0")),
+        unique=True # id の重複はエラー
+    ),
+    columns = {
+        "value01": pa.Column(int, checks=pa.Check.ge(0)),
+        "value02": pa.Column(int, checks=pa.Check.ge(0)),
+        "value03": pa.Column(int, checks=pa.Check.ge(0)),
+        "value04": pa.Column(str, checks=[
+            pa.Check(lambda values: value04_validate(values, df_data02))
+        ])
+    },
+    strict=True,    # 定義したカラムのみしか持たないか確認
+    coerce=True     # 強制型変換
+)
+data01_schema.validate(df_data01)
